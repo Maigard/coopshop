5402e8d6b1b8f681aab297fa9ea4a21e352bd661
Ken Schmidt <kenschmidt@gmail.com>
2013-03-30 18:42:58 -0700

    Adding order payment type migrations

---
+++ migrations/0004_auto__add_field_order_paymentType.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Order.paymentType'
+        db.add_column('coopshop_order', 'paymentType',
+                      self.gf('django.db.models.fields.CharField')(max_length=16, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Order.paymentType'
+        db.delete_column('coopshop_order', 'paymentType')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'coopshop.aboutimage': {
+            'Meta': {'object_name': 'AboutImage'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.AboutPage']"})
+        },
+        'coopshop.aboutpage': {
+            'Meta': {'object_name': 'AboutPage'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'defaultPage': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        'coopshop.category': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Category'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'coopshop.cycle': {
+            'Meta': {'ordering': "['date']", 'object_name': 'Cycle'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'delivery': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'coopshop.message': {
+            'Meta': {'ordering': "['date']", 'object_name': 'Message'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.MessageCategory']"}),
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'responded': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'coopshop.messagecategory': {
+            'Meta': {'ordering': "['name']", 'object_name': 'MessageCategory'},
+            'contact': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'coopshop.order': {
+            'Meta': {'object_name': 'Order'},
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'cycle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Cycle']"}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'delivered': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'deliveryFee': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nonmemberFee': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'paid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paymentId': ('django.db.models.fields.CharField', [], {'default': 'False', 'max_length': '32', 'null': 'True'}),
+            'paymentType': ('django.db.models.fields.CharField', [], {'max_length': '16', 'null': 'True', 'blank': 'True'}),
+            'processingFee': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'products': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['coopshop.Product']", 'through': "orm['coopshop.OrderItem']", 'symmetrical': 'False'}),
+            'subtotal': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'tax': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'total': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'coopshop.orderitem': {
+            'Meta': {'ordering': "['product__producer']", 'object_name': 'OrderItem'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Order']"}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Product']"}),
+            'quantity': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2'}),
+            'wholesalePrice': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'coopshop.producer': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Producer'},
+            'about': ('django.db.models.fields.TextField', [], {}),
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'contact': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'leadTime': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'markup': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '3', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'phone': ('django.contrib.localflavor.us.models.PhoneNumberField', [], {'max_length': '20'}),
+            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'zip': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        'coopshop.product': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Product'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Category']"}),
+            'cycles': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['coopshop.Cycle']", 'through': "orm['coopshop.ProductCycle']", 'symmetrical': 'False'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'leadTime': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'markup': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '3', 'blank': 'True'}),
+            'membershipExtension': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'membershipPayment': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'minimumPrice': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'producer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Producer']"}),
+            'size': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '3', 'blank': 'True'}),
+            'taxable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Unit']"}),
+            'unlimitedQuantity': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'wholesalePrice': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '2'})
+        },
+        'coopshop.productcycle': {
+            'Meta': {'object_name': 'ProductCycle'},
+            'cycle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Cycle']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Product']"}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'coopshop.setting': {
+            'Meta': {'object_name': 'Setting'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'value': ('django.db.models.fields.TextField', [], {})
+        },
+        'coopshop.unit': {
+            'Meta': {'object_name': 'Unit'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
+        },
+        'coopshop.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'address1': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'address2': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lifetimeMember': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'membershipBalance': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '10', 'decimal_places': '2'}),
+            'membershipExpires': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'phone': ('django.contrib.localflavor.us.models.PhoneNumberField', [], {'max_length': '20'}),
+            'producer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['coopshop.Producer']", 'null': 'True', 'blank': 'True'}),
+            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'}),
+            'stripeId': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'zip': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['coopshop']
